702127bdb886a2ebe6470f4acd4da2094ee77f1b
Super Writer <jatisari.innovation@gmail.com>
2016-12-22 09:37:10 +0700

    Initial commit

---
+++ app/src/main/java/net/agusharyanto/learnmaterialdesign/ListContentFragment.java
@@ -0,0 +1,96 @@
+package net.agusharyanto.learnmaterialdesign;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListContentFragment extends Fragment {
+
+
+    public ListContentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        // Set padding for Tiles (not needed for Cards/Lists!)
+        int tilePadding = getResources().getDimensionPixelSize(R.dimen.tile_padding);
+        recyclerView.setPadding(tilePadding, tilePadding, tilePadding, tilePadding);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        return recyclerView;
+    }
+    public static  class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView avator;
+        public TextView name;
+        public TextView description;
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_list, parent, false));
+            avator = (ImageView) itemView.findViewById(R.id.list_avatar);
+            name = (TextView) itemView.findViewById(R.id.list_title);
+            description = (TextView) itemView.findViewById(R.id.list_desc);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, DetailActivity.class);
+                    intent.putExtra(DetailActivity.EXTRA_POSITION, getAdapterPosition());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder>{
+        private static final int LENGTH = 18;
+        private final String[] mPlaces;
+        private final String[] mPlaceDesc;
+        private final Drawable[] mPlaceAvators;
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            mPlaces = resources.getStringArray(R.array.places);
+            mPlaceDesc = resources.getStringArray(R.array.place_desc);
+            TypedArray a = resources.obtainTypedArray(R.array.place_avator);
+            mPlaceAvators = new Drawable[a.length()];
+            for (int i = 0; i < mPlaceAvators.length; i++) {
+                mPlaceAvators[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()),parent);
+        }
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.avator.setImageDrawable(mPlaceAvators[position % mPlaceAvators.length]);
+            holder.name.setText(mPlaces[position % mPlaces.length]);
+            holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
+        }
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
+    }
+}
